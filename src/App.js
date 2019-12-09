eaecad09f2dfca7e6c7da42049a15142979b7a00
Calvin Wahyudi <cnwcareer@gmail.com>
2019-12-08 21:53:00 -0800

    Very basic table

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ReactTable from 'react-table-v6';
+import 'react-table-v6/react-table.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+  constructor(){
+    super();
+
+    this.state = {
+      bankBalance: ''
+    }
+  }
+
+  componentDidMount(){
+
+  }
+
+  render(){
+
+    const data = [
+      {
+        name: 'Tanner Linsley',
+        age: 26,
+        friend: {
+          name: 'Jason Maurer',
+          age: 23,
+        }
+      }
+    ]
+
+    const columns = [
+      {
+        Header: 'Name',
+        accessor: 'name' // String-based value accessors!
+      }, 
+      {
+        Header: 'Age',
+        accessor: 'age',
+        Cell: props => <span className='number'>{props.value}</span> // Custom cell components!
+      }, 
+      {
+        id: 'friendName', // Required because our accessor is not a string
+        Header: 'Friend Name',
+        accessor: d => d.friend.name // Custom value accessors!
+      }, 
+      {
+        Header: props => <span>Friend Age</span>, // Custom header components!
+        accessor: 'friend.age'
+      }
+    ]
+
+    return (
+      <div className='app'>
+        <ReactTable
+          data={data}
+          columns={columns} className='table'
+        />
+
+      </div>
+    );  
+  }
 }
 
 export default App;
